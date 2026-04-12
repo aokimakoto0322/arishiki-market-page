57fee1a538cdeaf8e7d8900471253d6fc805e9db
aokimakoto0322 <aokimakoto0322@gmail.com>
2026-04-12 16:51:31 +0900

    dockerでホットリロード有効化のため設定の修正

--- arishiki-market-web-app/next.config.ts
+++ arishiki-market-web-app/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Turbopack を有効化
+  turbopack: {},
+  // Docker 環境でのホットリロードを有効化
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 300, // チェック頻度
+        aggregateTimeout: 100, // 待機時間
+        ignored: ['**/node_modules', '**/.next', '**/.git'], // 監視除外
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
