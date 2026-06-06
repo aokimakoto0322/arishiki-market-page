0c84a926266b124b2cd094aedf59272bc1972933
AokiMakoto <aokimakoto0322@gmail.com>
2026-06-06 15:53:23 +0900

    アプリのコンセプト追加

--- arishiki-market-web-app/next.config.ts
+++ arishiki-market-web-app/next.config.ts
@@ -2,15 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // --- ここに画像を読み込むための設定を追加しました ---
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  // ----------------------------------------------
+
   // Turbopack を有効化
   turbopack: {},
   // Docker 環境でのホットリロードを有効化
   webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
-        poll: 300, // チェック頻度
-        aggregateTimeout: 100, // 待機時間
-        ignored: ['**/node_modules', '**/.next', '**/.git'], // 監視除外
+        poll: 300, 
+        aggregateTimeout: 100, 
+        ignored: ['**/node_modules', '**/.next', '**/.git'], 
       };
     }
     return config;
